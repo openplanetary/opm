42ac77de06cc98f89d30ccd2d55321065994a93a
Myles Lewando <CodeMacabre@users.noreply.github.com>
2019-03-03 09:51:14 +0000

    Add initial structure :building_construction:

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <main>
+          {/* Content here */}
+          <header>
+            <img src='./assets/op-logowtxt-white.svg' alt='OpenPlanetary logo' />
+          </header>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default App

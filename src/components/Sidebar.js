b73b94156006f9e086f079e69604e78166121943
Myles Lewando <49149591+mtlewando@users.noreply.github.com>
2019-05-06 10:31:12 +0100

    :clown_face: Add dummy search results

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,9 +13,9 @@ class Sidebar extends React.Component {
 
   handleSidebar () {
     if (this.state.isSidebarActive) {
-      document.body.classList.add('sidebar-active')
-    } else {
       document.body.classList.remove('sidebar-active')
+    } else {
+      document.body.classList.add('sidebar-active')
     }
     this.setState(state => ({
       isSidebarActive: !state.isSidebarActive
@@ -29,12 +29,27 @@ class Sidebar extends React.Component {
     return (
       <div className='sidebar-container'>
         <button className='sidebar-button' onClick={this.handleSidebar}>
-          <FontAwesomeIcon className='sidebar-caret' icon={faCaretLeft} transform={isSidebarActive ? { rotate: 180 } : { rotate: 0 }} />
+          <FontAwesomeIcon className='sidebar-caret' icon={faCaretLeft} transform={isSidebarActive ? { rotate: 0 } : { rotate: 180 }} />
         </button>
         <fieldset className='main-search'>
           <input name='search' placeholder='Search' type='text' />
           <button><FontAwesomeIcon icon={faSearch} /></button>
         </fieldset>
+        <div className='search-results'>
+          <h2>Results</h2>
+          <ul className='fa-ul'>
+            <li>
+              <span className='fa-li'><i className='fas fa-map-marker-alt' /></span>
+              <a href='#'>Olympus Mons</a>
+              <span className='description'>18.65&deg;N, 226.2&deg;E</span>
+            </li>
+            <li>
+              <span className='fa-li'><i className='fas fa-map-marker-alt' /></span>
+              <a href='#'>Valles Marineris</a>
+              <span className='description'>13.9&deg;S, 59.2&deg;W</span>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }

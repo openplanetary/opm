d62bb9ddabcfbbc1354ec99260804d9a33d521a4
Myles Lewando <49149591+mtlewando@users.noreply.github.com>
2019-05-15 20:57:07 +0100

    :label: Add prop types

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types'
 
 class SearchResult extends React.Component {
   constructor (props) {
@@ -18,10 +19,16 @@ class SearchResult extends React.Component {
           <FontAwesomeIcon icon={faMapMarkerAlt} />
         </span>
         <a href='#'>{name}</a>
-        <span className='description'>{long}&deg;, {lat}&deg;</span>
+        <span className='description'>{long.toFixed(3)}&deg;, {lat.toFixed(3)}&deg;</span>
       </li>
     )
   }
+}
+
+SearchResult.propTypes = {
+  name: PropTypes.string,
+  long: PropTypes.number,
+  lat: PropTypes.number
 }
 
 export default SearchResult

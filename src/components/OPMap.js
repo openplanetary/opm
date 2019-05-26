c9003fa34fc919b87dd545056ecef91befa4b973
Myles Lewando <49149591+mtlewando@users.noreply.github.com>
2019-05-26 12:46:39 +0100

    :sparkles: Add basemaps & labels overlay

--- src/components/OPMap.js
+++ src/components/OPMap.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map, TileLayer, ScaleControl } from 'react-leaflet'
+import { LayersControl, Map, ScaleControl, TileLayer } from 'react-leaflet'
 import PropTypes from 'prop-types'
 
 import SearchResultMarker from '../components/SearchResultMarker'
@@ -34,10 +34,57 @@ class OPMap extends React.Component {
     return (
       <Map center={position} zoom={this.state.zoom} maxBounds={this.state.maxBounds} maxBoundsViscosity={this.state.maxBoundsViscosity}>
         <ScaleControl maxWidth='200' metric imperial={false} />
-        <TileLayer
-          attribution={opmAttribution}
-          url='https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-1/0,1,2,3,4/{z}/{x}/{y}.png'
-        />
+        <LayersControl>
+          <LayersControl.BaseLayer name='OPM Mars Basemap v0.1' checked>
+            <TileLayer
+              attribution={opmAttribution}
+              tms={false}
+              url='https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-1/0,1,2,3,4/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='OPM Shaded Mars Surface Texture Map'>
+            <TileLayer
+              attribution={`Celestia/praesepe | ${opmAttribution}`}
+              tms
+              url='http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/celestia_mars-shaded-16k_global/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='OPM Shaded Grayscale MOLA Elevation'>
+            <TileLayer
+              attribution={`NASA/MOLA | ${opmAttribution}`}
+              tms
+              url='http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/mola-gray/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='OPM Shaded Colour MOLA Elevation'>
+            <TileLayer
+              attribution={`NASA/MOLA | ${opmAttribution}`}
+              tms
+              url='http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/mola-color/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='OPM Global Viking MDIM2.1 Colorized Mosaic'>
+            <TileLayer
+              attribution={`NASA/Viking/USGS | ${opmAttribution}`}
+              tms
+              url='http://s3-eu-west-1.amazonaws.com/whereonmars.cartodb.net/viking_mdim21_global/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name='OPM Global Hillshade Map'>
+            <TileLayer
+              attribution={`NASA/MOLA | ${opmAttribution}`}
+              tms
+              url='https://s3.us-east-2.amazonaws.com/opmmarstiles/hillshade-tiles/{z}/{x}/{y}.png'
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.Overlay name='OPM Mars Basemap (v0.1) Labels' checked>
+            <TileLayer
+              attribution={`${opmAttribution}`}
+              tms
+              url='https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-1/5/{z}/{x}/{y}.png'
+            />
+          </LayersControl.Overlay>
+        </LayersControl>
         {searchResults}
       </Map>
     )
